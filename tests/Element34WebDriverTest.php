0b57069aff0a3c1c63acc682da45c6bd93d08f4d
Davert <davert.php@resend.cc>
2014-02-27 02:53:12 +0200

    added Element34/Instaclick tests

--- tests/Element34WebDriverTest.php
+++ tests/Element34WebDriverTest.php
@@ -20,7 +20,6 @@ class Element34WebDriverTest extends PHPUnit_Framework_TestCase {
         $this->session->close();
     }
 
-
     protected $from = 'Kyiv';
     protected $fromInput = 'ky';
     protected $to = 'Lviv';
@@ -30,28 +29,80 @@ class Element34WebDriverTest extends PHPUnit_Framework_TestCase {
     public function testKyivLvivBooking()
     {
         $this->session->open('http://booking.uz.gov.ua/');
-        $this->fillStation('station_from', $this->fromInput);
+        $from = $this->fillStation('station_from', $this->fromInput);
         $this->pickStation('stations_from', $this->from);
-        $this->waitForUserInput();
+        $this->assertEquals("Kyiv", $from->attribute('value'));
 
+        $to = $this->fillStation('station_till', $this->toInput);
+        $this->pickStation('stations_till', $this->to);
+        $this->assertEquals("Lviv", $to->attribute('value'));
 
+        $this->forTomorrow();
+        $this->session->element('name', 'search')->click();
+
+        $result = $this->trains();
+
+        (new PHPWebDriver_WebDriverWait($this->session))->until(
+           function($session) {
+             return $session->element(PHPWebDriver_WebDriverBy::ID, 'ts_res')->displayed();
+           }
+        );
+
+        $trains = $result->elements(PHPWebDriver_WebDriverBy::TAG_NAME, 'tr');
+        $this->assertNotEmpty($trains);
+        $trainId = $trains[0]->element(PHPWebDriver_WebDriverBy::CLASS_NAME, 'num')->text();
+
+        $trains = $result->elements(PHPWebDriver_WebDriverBy::TAG_NAME, 'tr');
+        $place = $trains[0]->element(PHPWebDriver_WebDriverBy::CLASS_NAME, 'place');
+        $places = $place->elements(PHPWebDriver_WebDriverBy::TAG_NAME, 'div');
+        $this->assertNotEmpty($places);
+        $cheapest = end($places);
+        $cheapest->element(PHPWebDriver_WebDriverBy::TAG_NAME, 'button')->click();
+
+        (new PHPWebDriver_WebDriverWait($this->session))->until(
+           function($session) {
+             return count($session->elements(PHPWebDriver_WebDriverBy::CLASS_NAME, 'vToolsPopup'));
+           }
+        );
+
+        $popup = $this->session->element(PHPWebDriver_WebDriverBy::CLASS_NAME, 'vToolsPopup');
+        $this->assertTrue($popup->displayed());
+        $this->assertContains($trainId, $popup->element(PHPWebDriver_WebDriverBy::CLASS_NAME, 'vToolsPopupHeader')->text());
     }
+
+
+    protected function trains()
+    {
+        $result = $this->session->element('id', 'ts_res');
+        return $result->element(PHPWebDriver_WebDriverBy::TAG_NAME, 'tbody');
+    }
+
 
     protected function fillStation($el, $station)
     {
-        return $this->session->element(PHPWebDriver_WebDriverBy::NAME, $el)->sendKeys($station);
+        $el = $this->session->element(PHPWebDriver_WebDriverBy::NAME, $el);
+        $el->sendKeys($station);
+        return $el;
     }
 
     protected function pickStation($el, $station)
     {
         (new PHPWebDriver_WebDriverWait($this->session))->until(
            function($session) use ($el) {
-             return $session->element(PHPWebDriver_WebDriverBy::ID, $el);
+             return $session->element(PHPWebDriver_WebDriverBy::ID, $el)->displayed();
            }
         );
-        return $this->session->element('xpath', "./div[@title='$station']")->click();
-
+        return $this->session->element(PHPWebDriver_WebDriverBy::ID, $el)->element('xpath', "./div[@title='$station']")->click();
     }
+
+    protected function forTomorrow()
+    {
+        $el = $this->session->element(PHPWebDriver_WebDriverBy::ID, 'date_dep');
+        $el->sendKeys((new PHPWebDriver_WebDriverKeys('ControlKey'))->key.'a');
+        $el->sendKeys(date("m.d.Y", time() + 86400));
+        return $el;
+    }
+
 
 
     protected function waitForUserInput()
@@ -59,34 +110,4 @@ class Element34WebDriverTest extends PHPUnit_Framework_TestCase {
         if(trim(fgets(fopen("php://stdin","r"))) != chr(13)) return;
     }
 
-
-    
-//$this->wd->get('http://booking.uz.gov.ua/');
-//$from = $this->fillStation(WebDriverBy::name('station_from'), $this->fromInput);
-//$this->pickStattion(WebDriverBy::id('stations_from'), $this->from);
-//$this->assertEquals("Kyiv", $from->getAttribute('value'));
-//
-//$to = $this->fillStation(WebDriverBy::name('station_till'), $this->toInput);
-//$this->pickStattion(WebDriverBy::id('stations_till'), $this->to);
-//$this->assertEquals("Lviv", $to->getAttribute('value'));
-//
-//$this->forTomorrow();
-//
-//$this->wd->findElement(WebDriverBy::name('search'))->click();
-//
-//$result = $this->wd->findElement(WebDriverBy::id('ts_res'));
-//
-//(new WebDriverWait($this->wd, 20))
-//    ->until(WebDriverExpectedCondition::visibilityOf($result));
-//
-//$train = $this->firstTrainId($result);
-//$this->openCheapestPlaceForFirstTrain($result);
-//
-//(new WebDriverWait($this->wd, 10))
-//    ->until(WebDriverExpectedCondition::presenceOfAllElementsLocatedBy(WebDriverBy::className('vToolsPopup')));
-//
-//$popup = $this->wd->findElement(WebDriverBy::className('vToolsPopup'));
-//$this->assertTrue($popup->isDisplayed());
-//$this->assertContains($train, $popup->findElement(WebDriverBy::className('vToolsPopupHeader'))->getText());
-    
 }
--- tests/InstaclickWebDriverTest.php
+++ tests/InstaclickWebDriverTest.php
@@ -6,18 +6,18 @@ class InstaclickWebDriverTest extends PHPUnit_Framework_TestCase {
     /**
      * @var WebDriver\Session;
      */
-    protected $wd;
+    protected $session;
 
     public function setUp()
     {
         $wd_host = 'http://localhost:4444/wd/hub'; // this is the default
         $web_driver = new \WebDriver\WebDriver($wd_host);
-        $this->wd = $web_driver->session('firefox');
+        $this->session = $web_driver->session('firefox');
     }
 
     public function tearDown()
     {
-        $this->wd->close();
+        $this->session->close();
     }
 
 
@@ -29,13 +29,81 @@ class InstaclickWebDriverTest extends PHPUnit_Framework_TestCase {
 
     public function testKyivLvivBooking()
     {
-        $this->wd->open('http://booking.uz.gov.ua/');
-        $input = $this->wd->element('name', 'station_from');
-        $input->click('');
-        $input->postValue($this->fromInput);
-        $this->waitForUserInput();
+        $this->session->open('http://booking.uz.gov.ua/');
+        $from = $this->fillStation('station_from', $this->fromInput);
+        $this->pickStation('stations_from', $this->from);
+        $this->assertEquals("Kyiv", $from->attribute('value'));
 
+        $to = $this->fillStation('station_till', $this->toInput);
+        $this->pickStation('stations_till', $this->to);
+        $this->assertEquals("Lviv", $to->attribute('value'));
+
+        $this->forTomorrow();
+        $this->session->element('name', 'search')->click();
+
+        $result = $this->trains();
+
+        $this->session->timeouts()->wait(
+           function($session) {
+             return $session->element(\WebDriver\LocatorStrategy::ID, 'ts_res')->displayed();
+           }
+        , 20, 1, [$this->session]);
+
+        $trains = $result->elements(\Webdriver\LocatorStrategy::TAG_NAME, 'tr');
+        $this->assertNotEmpty($trains);
+        $trainId = $trains[0]->element(\Webdriver\LocatorStrategy::CLASS_NAME, 'num')->text();
+
+        $trains = $result->elements(\Webdriver\LocatorStrategy::TAG_NAME, 'tr');
+        $place = $trains[0]->element(\Webdriver\LocatorStrategy::CLASS_NAME, 'place');
+        $places = $place->elements(\Webdriver\LocatorStrategy::TAG_NAME, 'div');
+        $this->assertNotEmpty($places);
+        $cheapest = end($places);
+        $cheapest->element(\Webdriver\LocatorStrategy::TAG_NAME, 'button')->click();
+
+        $this->session->timeouts()->wait(
+           function($session) {
+             return count($session->elements(\Webdriver\LocatorStrategy::CLASS_NAME, 'vToolsPopup')) > 0;
+           }
+        , 20, 5, [$this->session]);
+
+        $popup = $this->session->element(\Webdriver\LocatorStrategy::CLASS_NAME, 'vToolsPopup');
+        $this->assertTrue($popup->displayed());
+        $this->assertContains($trainId, $popup->element(\Webdriver\LocatorStrategy::CLASS_NAME, 'vToolsPopupHeader')->text());
     }
+
+
+    protected function trains()
+    {
+        $result = $this->session->element('id', 'ts_res');
+        return $result->element(\Webdriver\LocatorStrategy::TAG_NAME, 'tbody');
+    }
+
+
+    protected function fillStation($el, $station)
+    {
+        $el = $this->session->element(\Webdriver\LocatorStrategy::NAME, $el);
+        $el->value(array('value' => array($station)));
+        return $el;
+    }
+
+    protected function pickStation($el, $station)
+    {
+        $this->session->timeouts()->wait(
+           function($session) use ($el) {
+             return $session->element(\Webdriver\LocatorStrategy::ID, $el)->displayed();
+           }
+        , 20, 1, [$this->session]);
+        return $this->session->element(\Webdriver\LocatorStrategy::ID, $el)->element('xpath', "./div[@title='$station']")->click();
+    }
+
+    protected function forTomorrow()
+    {
+        $el = $this->session->element(\Webdriver\LocatorStrategy::ID, 'date_dep');
+        $el->value(array('value' => array(\WebDriver\Key::CONTROL.'a')));
+        $el->value(array('value' => array(date("m.d.Y", time() + 86400))));
+        return $el;
+    }
+
 
     protected function waitForUserInput()
     {
@@ -43,33 +111,5 @@ class InstaclickWebDriverTest extends PHPUnit_Framework_TestCase {
     }
 
 
-    
-//$this->wd->get('http://booking.uz.gov.ua/');
-//$from = $this->fillStation(WebDriverBy::name('station_from'), $this->fromInput);
-//$this->pickStattion(WebDriverBy::id('stations_from'), $this->from);
-//$this->assertEquals("Kyiv", $from->getAttribute('value'));
-//
-//$to = $this->fillStation(WebDriverBy::name('station_till'), $this->toInput);
-//$this->pickStattion(WebDriverBy::id('stations_till'), $this->to);
-//$this->assertEquals("Lviv", $to->getAttribute('value'));
-//
-//$this->forTomorrow();
-//
-//$this->wd->findElement(WebDriverBy::name('search'))->click();
-//
-//$result = $this->wd->findElement(WebDriverBy::id('ts_res'));
-//
-//(new WebDriverWait($this->wd, 20))
-//    ->until(WebDriverExpectedCondition::visibilityOf($result));
-//
-//$train = $this->firstTrainId($result);
-//$this->openCheapestPlaceForFirstTrain($result);
-//
-//(new WebDriverWait($this->wd, 10))
-//    ->until(WebDriverExpectedCondition::presenceOfAllElementsLocatedBy(WebDriverBy::className('vToolsPopup')));
-//
-//$popup = $this->wd->findElement(WebDriverBy::className('vToolsPopup'));
-//$this->assertTrue($popup->isDisplayed());
-//$this->assertContains($train, $popup->findElement(WebDriverBy::className('vToolsPopupHeader'))->getText());
-    
+
 }
